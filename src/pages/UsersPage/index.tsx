918b6617c730dc962a4f2d72f2aa7e61d3adcb0d
ejunior95 <e.junior95@gmail.com>
2021-04-03 18:08:11 -0300

    [UPDATE]-> Estruturação de novas páginas

---
+++ src/pages/UsersPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const UsersPage: React.FC = () => {
+  return (
+      <Container>
+          
+      </Container>
+  );
+}
+
+export default UsersPage;
