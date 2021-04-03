955c86b7ee78c1c14a740f97edeea5d7de6c53eb
ejunior95 <e.junior95@gmail.com>
2021-04-03 17:51:06 -0300

    [UPDATE]-> Iniciando alterações no menu lateral da página inicial

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, Header } from './styles';
+import { GiStoneBlock } from "react-icons/gi";
+import { FiUsers } from "react-icons/fi";
+import { BsCreditCard } from "react-icons/bs";
+import { AiOutlineAudit } from "react-icons/ai";
+import { MdDashboard } from "react-icons/md";
 
 const HomePage: React.FC = () => {
   return(
       <Container>
+
+          <Header>
+            <div className="container-logo">
+              <h2 className="text-logo">Cred</h2>
+                <GiStoneBlock className="logo" />
+              <h2 className="text-logo">Rock</h2>
+            </div>
+            <ul>
+              <li className="active"><MdDashboard /> Página inicial</li>
+              <li className="inactive"><FiUsers /> Visualizar usuários</li>
+              <li className="inactive"><BsCreditCard /> Visualizar cartões disponiveis</li>
+              <li className="inactive"><AiOutlineAudit /> Visualizar auditoria </li>
+            </ul>
+          </Header>
+
       </Container>
   );
 }
--- src/pages/HomePage/styles.ts
+++ src/pages/HomePage/styles.ts
@@ -1,5 +1,55 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  height: 100vh;
+`;
+
+export const Header = styled.div`
+  height: 100%;
+  width: 45vh;
+  background-color: #ffffff;
+
+  ul {
+      display: flex;
+      flex-direction: column;
+      align-items: left;
+      justify-content: space-around;
+      height: 50%;
+      padding-left: 1vh;
+  }
+  ul li {
+    padding: 1vh;
+    border-radius: 5px;
+    border: 2px solid transparent;
+    cursor: pointer;
+  }
+  ul li.active {
+    width: 100%;
+    background-color: #238636;
+    color: #ffffff;
+    transition: .2s ease-in-out;
+  }
+  ul li.inactive:hover {
+    width: 80%;
+    border: 2px solid #238636;
+    background-color: #26943C;
+    color: #ffffff;
+    transition: .2s ease-in-out;
+  }
+  .container-logo {
+    height: 20%;
+    padding-top: 2%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+  }
+  .logo {
+    font-size: 50px;
+    color: #000000;
+  }
+  .text-logo {
+    color: #000000;
+    padding: 10px;
+  }
 `;
