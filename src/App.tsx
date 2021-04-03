049e4f00dc3725883a0829815384b2286d73cc6b
ejunior95 <e.junior95@gmail.com>
2021-04-02 22:59:55 -0300

    [UPDATE]-> Atualização de estruturas das páginas, instalação dos pacotes styled-components e react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -19,21 +17,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>{JSON.stringify(users)}</p>
-      </header>
+        <h1>teste</h1>
+        {/* <p>{JSON.stringify(users)}</p> */}
     </div>
   );
 }
---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const HomePage: React.FC = () => {
+  return(
+      <Container>
+          
+      </Container>
+  );
+}
+
+export default HomePage;
