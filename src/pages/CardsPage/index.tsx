8d4d778df5350ad958c3d80067976a602ababb72
ejunior95 <e.junior95@gmail.com>
2021-04-11 00:06:23 -0300

    [UPDATE]-> Implementando o componente BannerInfoUser nas páginas Users e Cards

--- src/pages/CardsPage/index.tsx
+++ src/pages/CardsPage/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import BannerInfoUser from '../../components/BannerInfoUser';
 import { Container } from './styles';
 
 const CardsPage: React.FC = () => {
   return (
       <Container>
-          
-
+          <BannerInfoUser />
+            <div className="conteudo">
+              
+            </div>
       </Container>
   );
 }
--- src/pages/UsersPage/index.tsx
+++ src/pages/UsersPage/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import BannerInfoUser from '../../components/BannerInfoUser';
 import { Container } from './styles';
 
 const UsersPage: React.FC = () => {
   return (
       <Container>
-       
+        <BannerInfoUser />
+            <div className="conteudo">
+              
+            </div>
       </Container>
   );
 }
