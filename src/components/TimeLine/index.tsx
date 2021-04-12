871d5516d5ad6c32914820a365867856dda69a25
ejunior95 <e.junior95@gmail.com>
2021-04-12 04:36:34 -0300

    [UPDATE]-> Implementação das informações do componente Timeline

--- src/components/TimeLine/index.tsx
+++ src/components/TimeLine/index.tsx
@@ -1,40 +1,67 @@
 import React from 'react';
 import { Container } from './styles';
 
-const TimeLine: React.FC = () => {
+
+interface IProps {
+  audits: IAudit[]
+}
+
+interface ICard {
+  id: number;
+  metadatas: { 
+      name?: string;
+      digits: number;
+      limit: number;
+  };
+  createdAt: string;
+  updatedAt?: string;
+  status: string;
+}
+
+interface IAudit {
+  type: string;
+  createdAt: string;
+  before: ICard;
+  after: ICard
+}
+
+const TimeLine = (props:IProps) => {
+  
+  const {
+    audits
+  } = props
+
   return (
-      <Container>
+  
+  <Container>
           
         <div className="timeline">
 
           <ul>
-
+            {audits.map(audit => (
+              <>
               <li>
                   <div className="content">
-                      <h3>Whats is Lorem Ipsum?</h3>
-                      <p>It was popularised in 
-                        the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-                        and more recently with desktop publishing software like Aldus PageMaker including 
-                        versions of Lorem Ipsum.</p>
+                      <h3>{audit.type}</h3>
+                      <p><strong>DEPOIS</strong></p>
+                      <p>Status: {audit.after.status}</p>
                   </div>
                   <div className="time">
-                      <h4>11/04/2021 - 11:20</h4>
+                      <h4>{audit.createdAt}</h4>
                   </div>
               </li>
-                
-              
               <li>
                   <div className="content">
-                      <h3>Whats is Lorem Ipsum?</h3>
-                      <p>It was popularised in 
-                        the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-                        and more recently with desktop publishing software like Aldus PageMaker including 
-                        versions of Lorem Ipsum.</p>
+                      <h3>{audit.type}</h3>
+                      <p><strong>ANTES</strong></p>
+                      <p>Status: {audit.before.status}</p>
                   </div>
                   <div className="time">
-                      <h4>11/04/2021 - 11:20</h4>
+                      <h4>{audit.createdAt}</h4>
                   </div>
               </li>
+              </>
+                ))}
               
 
               <div className="intervalo" />
--- src/pages/AuditPage/index.tsx
+++ src/pages/AuditPage/index.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container } from './styles';
 import BannerInfoUser from '../../components/BannerInfoUser';
 import TimeLine from '../../components/TimeLine';
-import { Container } from './styles';
+import { useEffect } from 'react';
+import { api } from '../../services/api';
+
+interface ICard {
+  id: number;
+  metadatas: { 
+      name?: string;
+      digits: number;
+      limit: number;
+  };
+  createdAt: string;
+  updatedAt?: string;
+  status: string;
+}
+
+interface IAudit {
+  type: string;
+  createdAt: string;
+  before: ICard;
+  after: ICard
+}
 
 const AuditPage: React.FC = () => {
+
+  const [audits, setAudits] = useState<IAudit[]>([] as IAudit[])
+
+
+  useEffect(() => {
+    api.get('audits')
+    .then(res => setAudits(res.data))
+  },[])
+
+
   return(
       <Container>
           <BannerInfoUser />
           <div className="conteudo">
               <div className="bloco-detalhes-info">
-                <TimeLine />
+                <TimeLine audits={audits} />
               </div>
           </div>
          
